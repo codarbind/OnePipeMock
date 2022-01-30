ebc7afb493af03275dfad07b013be4729954294c
Abdwahab <adekolaabdwahababisoye@gmail.com>
2022-01-30 02:27:31 -0800

    collect route

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ require('dotenv').config()
 const account = require('./controllers/account')
 const balance = require('./controllers/balance')
 const statement = require('./controllers/statement')
-
+const collect = require('./controllers/collect')
 
 app.use(account)
 app.use(balance)
 app.use(statement)
-
+app.use(collect)
 
 
 
