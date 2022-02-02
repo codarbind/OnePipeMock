4edcbe5bd3737bd7103355826cba276adbd029ce
Abdwahab <adekolaabdwahababisoye@gmail.com>
2022-02-02 04:35:47 -0800

    integrating front end

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+const cors = require('cors')
 require('dotenv').config()
 
 const account = require('./controllers/account')
@@ -8,6 +9,7 @@ const balance = require('./controllers/balance')
 const statement = require('./controllers/statement')
 const collect = require('./controllers/collect')
 
+app.use(cors())
 app.use(account)
 app.use(balance)
 app.use(statement)
